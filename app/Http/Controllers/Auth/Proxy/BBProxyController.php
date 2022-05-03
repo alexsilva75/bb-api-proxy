073dde323dfb97e3ce737fef325861ef00bc8e7b
Alexsandro Silva Santos <alexsandro.santos@gm.local>
2022-05-03 15:37:49 -0300

    first commit

---
+++ app/Http/Controllers/Auth/Proxy/BBProxyController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Proxy;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class BBProxyController extends Controller
+{
+    //
+    private $basic = "Basic #######";
+
+    private $baseURL = 'https://api.hm.bb.com.br/cobrancas/v2';
+    private $apiKey = '#####';
+    public function getToken(Request $request){
+
+        //dd($request->all());
+        $response = Http::withHeaders(
+            [
+                'Authorization' => $this->basic,
+
+            ]
+        )->acceptJson()
+        ->asForm()
+        ->post('https://oauth.hm.bb.com.br/oauth/token',
+            [
+                'grant_type' => "client_credentials",
+                'scope' => "cobrancas.boletos-info cobrancas.boletos-requisicao",
+            ]
+        );
+
+        $tokenData = json_decode($response->getBody()->getContents());
+
+        Cache::put('apiToken',$tokenData->access_token, 9);
+        //dd($tokenData->access_token);
+
+        return response()->json(['data' =>$response->getBody()->getContents()]);
+    }
+
+    public function boletos(Request $request){
+        if(!Cache::has('apiToken')){
+            $this->getAndStoreToken();
+        }
+
+        $token = Cache::get('apiToken');
+
+        $data = $request->json()->all();
+
+        //return response()->json(['pagador' => $data['pagador']]);
+        //dd($data['numeroConvenio']);
+
+
+        $response = Http::withHeaders(
+            [
+                'Authorization' => "Bearer $token",
+
+            ]
+        )->acceptJson()
+        //->asForm()
+        ->post("{$this->baseURL}/boletos?gw-dev-app-key={$this->apiKey}",
+             json_encode($data )
+        );
+
+
+
+
+
+        return json_decode($response->getBody()->getContents(), JSON_UNESCAPED_SLASHES);
+    }
+
+
+    private function getAndStoreToken(){
+        $response = Http::withHeaders(
+            [
+                'Authorization' => $this->basic,
+
+            ]
+        )->acceptJson()
+        ->asForm()
+        ->post('https://oauth.hm.bb.com.br/oauth/token',
+            [
+                'grant_type' => "client_credentials",
+                'scope' => "cobrancas.boletos-info cobrancas.boletos-requisicao",
+            ]
+        );
+
+        $tokenData = json_decode($response->getBody()->getContents());
+
+        Cache::put('apiToken',$tokenData->access_token, 9);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/v1/token', [\App\Http\Controllers\Auth\Proxy\BBProxyController::class, 'getToken']);
+Route::post('/v1/boletos', [\App\Http\Controllers\Auth\Proxy\BBProxyController::class, 'boletos']);
